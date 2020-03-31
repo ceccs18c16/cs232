a70a26b1102f8a625699c0c5479ebf3119377112
ceccs18c16 <60248788+ceccs18c16@users.noreply.github.com>
2020-03-31 18:46:07 +0530

    Add files via upload

---
+++ 2903/subtract.pl
@@ -0,0 +1,7 @@
+print "Enter first value :\t";
+$a=<>;
+print "Enter second value :\t";
+$b=<>;
+print "Value of a =$a and Value of b = $b";
+$c = $a - $b;
+print 'Value of c is =' . $c ."\n";
---
+++ 2903/uppercase.pl
@@ -0,0 +1,4 @@
+print "Enter the string:\t"; 
+$a=<>;
+print "\U $a ";
+print "\Uwelcoming to programming\n"
